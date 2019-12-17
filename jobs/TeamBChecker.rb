1a19e87957b8a35c7f975b11fd9154900ac7b185
Martin White <martin.white@codecomputerlove.com>
2019-12-17 11:26:05 +0000

    Added secrets separation

--- jobs/TeamBChecker.rb
+++ jobs/TeamBChecker.rb
@@ -1,8 +1,8 @@
 # Populate the graph with some random points
 total =0;
-SCHEDULER.every '10s' do   
+SCHEDULER.every '300s' do   
   puts "Team B: running checker"
-  new_total = get_number_of_correct_responses("http://localhost:8080") 
+  new_total = get_number_of_correct_responses(AppSecrets.get_b_endpoint) 
   send_event('convergenceB', { current: new_total})
   puts "Team B: Scored" + new_total.to_s
   total = total + new_total
--- jobs/TeamCChecker.rb
+++ jobs/TeamCChecker.rb
@@ -1,11 +1,11 @@
-# # Populate the graph with some random points
-# total =0;
-# SCHEDULER.every '10s' do   
-#   puts "Team C: running checker"
-#   new_total = get_number_of_correct_responses("http://localhost:8080") 
-#   send_event('convergenceC', { current: new_total})
-#   puts "Team C: Scored" + new_total.to_s
-#   total = total + new_total
-#   puts "Team C: Grand Total" + total.to_s
-#   send_event('convergenceCTotal', { current: total})
-# end
+# Populate the graph with some random points
+total =0;
+SCHEDULER.every '10s' do     
+  puts "Team C: running checker"
+  new_total = get_number_of_correct_responses(AppSecrets.get_c_endpoint) 
+  send_event('convergenceC', { current: new_total})
+  puts "Team C: Scored" + new_total.to_s
+  total = total + new_total
+  puts "Team C: Grand Total" + total.to_s
+  send_event('convergenceCTotal', { current: total})
+end
